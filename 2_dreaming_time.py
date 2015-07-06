4ff1e2cb79a395594eaaab640089361b1e3a9cf0
Peter Foley <pefoley2@pefoley.com>
2015-07-06 19:36:11 -0400

    pep8

--- 2_dreaming_time.py
+++ 2_dreaming_time.py
@@ -6,112 +6,105 @@ import os
 import errno
 
 # imports and basic notebook setup
-from cStringIO import StringIO
 import numpy as np
 import scipy.ndimage as nd
 import PIL.Image
-from google.protobuf import text_format
-
+import sys
+sys.path.append('../caffe/distribute/python')
 import caffe
 
-def showarray(a, fmt='jpeg'):
-    a = np.uint8(np.clip(a, 0, 255))
-    f = StringIO()
-    PIL.Image.fromarray(a).save(f, fmt)
-    display(Image(data=f.getvalue()))
-
-
-#Load DNN
-model_path = 'caffe/models/bvlc_googlenet/' # substitute your path here
-net_fn   = model_path + 'deploy.prototxt'
+# Load DNN
+model_path = 'caffe/models/bvlc_googlenet/'  # substitute your path here
+net_fn = model_path + 'deploy.prototxt'
 param_fn = model_path + 'bvlc_googlenet.caffemodel'
 
-# Patching model to be able to compute gradients.
-# Note that you can also manually add "force_backward: true" line to "deploy.prototxt".
-model = caffe.io.caffe_pb2.NetParameter()
-text_format.Merge(open(net_fn).read(), model)
-model.force_backward = True
-open('tmp.prototxt', 'w').write(str(model))
+net = caffe.Classifier(net_fn, param_fn,
+                       # ImageNet mean, training set dependent
+                       mean=np.float32([104.0, 116.0, 122.0]),
+                       channel_swap=(2, 1, 0))  # the reference model has channels in BGR order instead of RGB
 
-net = caffe.Classifier('tmp.prototxt', param_fn,
-                       mean = np.float32([104.0, 116.0, 122.0]), # ImageNet mean, training set dependent
-                       channel_swap = (2,1,0)) # the reference model has channels in BGR order instead of RGB
+# a couple of utility functions for converting to and from Caffe's input
+# image layout
 
-# a couple of utility functions for converting to and from Caffe's input image layout
+
 def preprocess(net, img):
     return np.float32(np.rollaxis(img, 2)[::-1]) - net.transformer.mean['data']
+
+
 def deprocess(net, img):
     return np.dstack((img + net.transformer.mean['data'])[::-1])
 
 
-#Make dreams
+# Make dreams
 def make_step(net, step_size=1.5, end='inception_4c/output', jitter=32, clip=True):
     '''Basic gradient ascent step.'''
 
-    src = net.blobs['data'] # input image is stored in Net's 'data' blob
+    src = net.blobs['data']  # input image is stored in Net's 'data' blob
     dst = net.blobs[end]
 
-    ox, oy = np.random.randint(-jitter, jitter+1, 2)
-    src.data[0] = np.roll(np.roll(src.data[0], ox, -1), oy, -2) # apply jitter shift
+    ox, oy = np.random.randint(-jitter, jitter + 1, 2)
+    # apply jitter shift
+    src.data[0] = np.roll(np.roll(src.data[0], ox, -1), oy, -2)
 
     net.forward(end=end)
     dst.diff[:] = dst.data  # specify the optimization objective
     net.backward(start=end)
     g = src.diff[0]
     # apply normalized ascent step to the input image
-    src.data[:] += step_size/np.abs(g).mean() * g
+    src.data[:] += step_size / np.abs(g).mean() * g
 
-    src.data[0] = np.roll(np.roll(src.data[0], -ox, -1), -oy, -2) # unshift image
+    src.data[0] = np.roll(
+        np.roll(src.data[0], -ox, -1), -oy, -2)  # unshift image
 
     if clip:
         bias = net.transformer.mean['data']
-        src.data[:] = np.clip(src.data, -bias, 255-bias)
+        src.data[:] = np.clip(src.data, -bias, 255 - bias)
+
 
 def deepdream(net, base_img, iter_n=10, octave_n=4, octave_scale=1.4, end='inception_4c/output', clip=True, **step_params):
     # prepare base images for all octaves
     octaves = [preprocess(net, base_img)]
-    for i in xrange(octave_n-1):
-        octaves.append(nd.zoom(octaves[-1], (1, 1.0/octave_scale,1.0/octave_scale), order=1))
+    for i in range(octave_n - 1):
+        octaves.append(
+            nd.zoom(octaves[-1], (1, 1.0 / octave_scale, 1.0 / octave_scale), order=1))
 
     src = net.blobs['data']
-    detail = np.zeros_like(octaves[-1]) # allocate image for network-produced details
+    # allocate image for network-produced details
+    detail = np.zeros_like(octaves[-1])
     for octave, octave_base in enumerate(octaves[::-1]):
         h, w = octave_base.shape[-2:]
         if octave > 0:
             # upscale details from the previous octave
             h1, w1 = detail.shape[-2:]
-            detail = nd.zoom(detail, (1, 1.0*h/h1,1.0*w/w1), order=1)
+            detail = nd.zoom(detail, (1, 1.0 * h / h1, 1.0 * w / w1), order=1)
 
-        src.reshape(1,3,h,w) # resize the network's input image size
-        src.data[0] = octave_base+detail
-        for i in xrange(iter_n):
+        src.reshape(1, 3, h, w)  # resize the network's input image size
+        src.data[0] = octave_base + detail
+        print("octave %d %s" % (octave, end))
+        for i in range(iter_n):
             make_step(net, end=end, clip=clip, **step_params)
-
-            # visualization
-            vis = deprocess(net, src.data[0])
-            if not clip: # adjust image contrast if clipping is disabled
-                vis = vis*(255.0/np.percentile(vis, 99.98))
-            showarray(vis)
-            print octave, i, end, vis.shape
-            clear_output(wait=True)
+            sys.stdout.write("%d " % i)
+            sys.stdout.flush()
+        print("")
 
         # extract details produced on the current octave
-        detail = src.data[0]-octave_base
+        detail = src.data[0] - octave_base
     # returning the resulting image
     return deprocess(net, src.data[0])
 
 # own functions
-def morphPicture(filename1,filename2):
+
+
+def morphPicture(filename1, filename2):
     img1 = PIL.Image.open(filename1)
     img2 = PIL.Image.open(filename2)
     return PIL.Image.blend(img1, img2, 0.5)
 
-layersloop = ['inception_4c/output','inception_4d/output',
+layersloop = ['inception_4c/output', 'inception_4d/output',
               'inception_4e/output', 'inception_5a/output',
-              'inception_5b/output','inception_5a/output',
-              'inception_4e/output','inception_4d/output',
+              'inception_5b/output', 'inception_5a/output',
+              'inception_4e/output', 'inception_4d/output',
               'inception_4c/output']
-
 
 
 def make_sure_path_exists(path):
@@ -125,17 +118,19 @@ def make_sure_path_exists(path):
         if exception.errno != errno.EEXIST:
             raise
 
-def main(input,output):
+
+def main(input, output):
     make_sure_path_exists(input)
     make_sure_path_exists(output)
 
-    frame = np.float32(PIL.Image.open(input+'/0001.jpg'))
+    frame = np.float32(PIL.Image.open(input + '/0001.jpg'))
     frame_i = 0
-    for i in xrange(frame_i,2980):
-        frame = deepdream(net, frame, end=layersloop[frame_i % len(layersloop)],iter_n = 5)
-        saveframe=input+"/%04d.jpg"%frame_i
+    for i in range(frame_i, 2980):
+        frame = deepdream(
+            net, frame, end=layersloop[frame_i % len(layersloop)], iter_n=5)
+        saveframe = input + "/%04d.jpg" % frame_i
         PIL.Image.fromarray(np.uint8(frame)).save(saveframe)
-        newframe=output+"/%04d.jpg"%frame_i
+        newframe = output + "/%04d.jpg" % frame_i
         frame = morphPicture(saveframe, newframe)
         frame = np.float32(frame)
         frame_i += 1
@@ -143,9 +138,10 @@ def main(input,output):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Dreaming in videos.')
-    parser.add_argument('-i','--input', help='Input directory where extracted frames are stored',required=True)
-    parser.add_argument('-o','--output',help='Output directory where processed frames are to be stored', required=True)
+    parser.add_argument(
+        '-i', '--input', help='Input directory where extracted frames are stored', required=True)
+    parser.add_argument(
+        '-o', '--output', help='Output directory where processed frames are to be stored', required=True)
     args = parser.parse_args()
 
     main(args.input, args.output)
-
